d32a4c6d14641fe5003583c7497303b5fb31bbf8
Sumon <msk.sumon.kaysar@gmail.com>
2025-06-20 13:57:28 +0600

    added mongodb and connection

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+let server: Server;
+
+const port = config.port || 5000;
+
 async function main() {
-    
+  try {
+    await mongoose.connect(config.db_url as string);
+    server = app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+main().catch((err) => console.log(err));
